6e72bb1c0511328af2fd76cf7dd84d94a450a9cf
Stivyx <stivyxx@gmail.com>
2017-05-25 16:11:19 +0200

    Installation des addons pour le projet

---
+++ wp-content/themes/montheme/page-templates/page-accueil.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Page d'accueil
+*/
+
+get_header(); ?>
+
+<div class="main-wrap page-accueil" role="main">
+
+		<!-- Accueil header -->
+		<section class="accueil_header">
+			<figure>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/la-coop-vu-du-ciel.jpg" alt="">
+				<figcaption>
+					<h1></h1>
+					<p></p>
+					<p></p>
+				</figcaption>
+			</figure>
+		</section>
+
+</div>
+
+<?php get_footer();
